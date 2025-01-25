639286a6cc3f804d51fcdbfbd173efdf9321f3e2
sumitwatkar <watkar.sumit@gmail.com>
2025-01-25 19:26:49 +0530

    s3 remote added

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\POCs\Sentiment-Analysis\myenv
+set VIRTUAL_ENV=D:\Projects\Sentiment-Analysis\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
